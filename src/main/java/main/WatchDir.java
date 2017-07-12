99e2c857505be2a447cdd036bb37f4940efbd264
mcaikovs <marian.caikovski@gmail.com>
2017-07-12 14:09:53 +0200

    import works

---
+++ src/main/java/main/WatchDir.java
@@ -0,0 +1,112 @@
+package main;
+
+
+import java.nio.file.*;
+import static java.nio.file.StandardWatchEventKinds.*;
+import java.io.*;
+import java.util.*;
+
+/**
+ * Example to watch a directory (or tree) for changes to files.
+ */
+public class WatchDir {
+
+    private final WatchService watcher;
+    private final Map<WatchKey, Path> keys;
+
+    @SuppressWarnings("unchecked")
+    static <T> WatchEvent<T> cast(WatchEvent<?> event) {
+        return (WatchEvent<T>) event;
+    }
+
+    /**
+     * Register the given directory with the WatchService
+     */
+    private void register(Path dir) throws IOException {
+        WatchKey key = dir.register(watcher, ENTRY_CREATE);
+
+        Path prev = keys.get(key);
+        if (prev == null) {
+            System.out.format("register: %s\n", dir);
+        } else {
+            if (!dir.equals(prev)) {
+                System.out.format("update: %s -> %s\n", prev, dir);
+            }
+        }
+
+        keys.put(key, dir);
+    }
+
+    /**
+     * Register the given directory, and all its sub-directories, with the
+     * WatchService.
+     */
+    /**
+     * Creates a WatchService and registers the given directory
+     */
+    WatchDir(Path dir) throws IOException {
+        this.watcher = FileSystems.getDefault().newWatchService();
+        this.keys = new HashMap<>();
+        register(dir);
+    }
+
+    /**
+     * Process all events for keys queued to the watcher
+     */
+    void processEvents() {
+        for (;;) {
+
+            // wait for key to be signalled
+            WatchKey key;
+            try {
+                key = watcher.take();
+            } catch (InterruptedException x) {
+                return;
+            }
+
+            Path dir = keys.get(key);
+            if (dir == null) {
+                System.err.println("WatchKey not recognized!!");
+                continue;
+            }
+
+            for (WatchEvent<?> event : key.pollEvents()) {
+                WatchEvent.Kind kind = event.kind();
+
+                // TBD - provide example of how OVERFLOW event is handled
+                if (kind == OVERFLOW) {
+                    continue;
+                }
+
+                // Context for directory entry event is the file name of entry
+                WatchEvent<Path> ev = cast(event);
+                Path name = ev.context();
+                Path child = dir.resolve(name);
+
+                // print out event
+               System.out.println(event.kind().name()+"; "+ child+"; "+name);
+
+            }
+
+            // reset key and remove from set if directory no longer accessible
+            boolean valid = key.reset();
+            if (!valid) {
+                keys.remove(key);
+
+                // all directories are inaccessible
+                if (keys.isEmpty()) {
+                    break;
+                }
+            }
+        }
+    }
+
+    static String EXCEL_DIR = "H:\\cbioportal_import";
+
+    public static void main(String[] args) throws IOException {
+
+        // register directory and process its events
+        Path dir = Paths.get(EXCEL_DIR);
+        new WatchDir(dir).processEvents();
+    }
+}
